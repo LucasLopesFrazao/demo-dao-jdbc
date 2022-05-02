7ce8ec0a5d1080fc9ddc9fe0d930ef5523bd36a4
LucasLopesFrazao <211031771@aluno.unb.br>
2022-05-02 08:18:45 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Department obg = new Department(1, "Books");
+		System.out.println(obg);
+
+	}
+
+}
