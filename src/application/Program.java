96b40bcffb280ac595a14e803a132b57ee1ae880
LucasLopesFrazao <211031771@aluno.unb.br>
2022-05-02 08:25:36 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obg = new Department(1, "Books");
-		System.out.println(obg);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obg);
+		
+		System.out.println(seller);
 
 	}
 
